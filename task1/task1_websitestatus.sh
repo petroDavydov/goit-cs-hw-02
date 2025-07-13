8fd0137551f60203a101d30d7db62e40b6c5bc2a
Petro Davydov <arturshopengauergik@gmail.com>
2025-07-13 12:43:54 +0300

    add task1

--- task1/task1_websitestatus.sh
+++ task1/task1_websitestatus.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-WEBSITES=("https://google.com", "https://facebook.com","https://twitter.com", "https://github.com", "https://copilot.microsoft.com", "<https://www.codewars.com", "https://academy.hackthebox.com/", "https://parrotsec.org/", "https://hackthebox.store/", "https://portswigger.net/") 
+WEBSITES=("https://google.com" "https://facebook.com" "https://twitter.com" "https://github.com" "https://copilot.microsoft.com" "https://www.codewars.com" "https://academy.hackthebox.com/" "https://parrotsec.org/" "https://hackthebox.store/" "https://portswigger.net/") 
 
-LOG_FILE="/task1/websitestatus.log"
+LOG_FILE="./websitestatus.log"
+if [ ! -d "$(dirname "$LOG_FILE")" ]; then
+    mkdir -p "$(dirname "$LOG_FILE")"
+fi
 
-REQUESTS_CURL="curl -I -s -o /dev/null -w '%{http_code}' -L $WEBSITES" 
-
- > "$log_file"
+true > "$LOG_FILE"
 
 for website in "${WEBSITES[@]}"; do
-    status_code=$REQUESTS_CURL
-    echo "$website: $status_code" >> "$LOG_FILE"
+    status_code=$(curl -I -s -o /dev/null -w "%{http_code}" -L "$website")
+    echo "$website: $status_code - $(date '+%d.%m.%Y %H:%M:%S')"
 
-    if [ $status_code -eq 200 ]; then
-        echo "$website is up and running"
+    if [ "$status_code" -eq 200 ]; then
+        echo "$website is UP" >> "$LOG_FILE"
+        echo -e "\e[32m$website is UP and RUNNING\e[0m"
     else
-        echo "$website is down"
+        echo "$website is DOWN" >> "$LOG_FILE"
+        echo -e "\e[31m$website is DOWN\e[0m"
     fi
 done
 
-echo "Website status log saved to $LOG_FILE"
+echo -e "\e[34mWebsite status log saved to $LOG_FILE\e[0m"
    
