c7b0b19d20dcf6ef1d837c38ee7788425f7ddf03
Petro Davydov <arturshopengauergik@gmail.com>
2025-07-20 16:01:32 +0300

    correct 5

--- task2/conf/db.py
+++ task2/conf/db.py
@@ -4,7 +4,7 @@ import os
 
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_HOST = os.getenv("POSTGRES_HOST", "lovcalhost")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DB_PORT = os.getenv("POSTGRES_PORT", "5432")
 DB_NAME = os.getenv("POSTGRES_DB")
 
